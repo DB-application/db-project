2975f11052a530cf0173b96c8908c914b14fd4cb
EldarMuhamethanov <eldarmy@mail.ru>
2021-11-06 22:49:49 +0300

    Запилил mapAtom переиспользовал его в userAtom, eventAtom, notesAtom

--- frontend/src/users/usersAtom.ts
+++ frontend/src/users/usersAtom.ts
@@ -1,45 +1,23 @@
-import {declareAction, declareAtom} from "@reatom/core";
 import {UserData} from "../common/UserData";
-import {loadUsers} from "./loadUsers";
+import {declareMapAtom} from "../core/reatom/declareMapAtom";
 
-type UsersMap = {
-    [item: string]: UserData,
-}
-
-const removeUser = declareAction<string>()
-const updateUser = declareAction<UserData>()
-const initUsers = declareAction<Array<UserData>>()
-
-const usersAtom = declareAtom<UsersMap>('usersAtom', {}, on => [
-    on(loadUsers.done, (state, users) => {
-        const newUsers = {...state}
-        users.forEach(user => newUsers[user.id] = user)
-        return newUsers
-    }),
-    on(updateUser, (state, user) => ({
-        ...state,
-        [user.id]: user,
-    })),
-    on(removeUser, (state, eventId) => {
-        const newUsers = {
-            ...state,
-        }
-        delete newUsers[eventId]
-        return newUsers
-    }),
-])
+const {
+    atom: usersAtom,
+    updateItems: updateUsers,
+    removeItems: removeUsers,
+    updateItem: updateUser,
+} = declareMapAtom<UserData>(
+    'usersAtom',
+    user => user.id,
+)
 
 const usersActions = {
-    removeUser,
+    removeUsers,
     updateUser,
-    initUsers,
+    updateUsers,
 }
 
 export {
     usersAtom,
     usersActions,
-}
-
-export type {
-    UsersMap,
 }
--- frontend/src/workspace/viewmodel/notes/notes.tsx
+++ frontend/src/workspace/viewmodel/notes/notes.tsx
@@ -1,51 +1,22 @@
 import {combine, declareAction, declareAtom, map} from "@reatom/core";
 import {Router} from "../../../core/router/router";
+import {declareMapAtom} from "../../../core/reatom/declareMapAtom";
+import {CalendarEvent} from "../calendar/calendar";
 
 type Note = {
     noteId: string,
     title: string,
 }
 
-type Notes = {
-    [item: string]: Note,
-}
-
-const removeNote = declareAction<string>(
-    (noteId, store) => {
-        const selectedNote = store.getState(selectedNoteAtom)
-        if (noteId === selectedNote) {
-            const notes = store.getState(orderedNotesAtom)
-            const notesCount = notes.length
-            if (notesCount > 0) {
-                store.dispatch(setSelectedNote(notes[0].noteId))
-                return
-            }
-            store.dispatch(setSelectedNote(null))
-            Router.Workspace.open()
-        }
-    }
+const {
+    atom: notesAtom,
+    removeItems: removeNotes,
+    updateItem: updateNote,
+    updateItems: updateNotes,
+} = declareMapAtom<Note>(
+    'sidebar.notes',
+    (note => note.noteId),
 )
-const updateNote = declareAction<Note>()
-const initNotes = declareAction<Array<Note>>()
-
-const notesAtom = declareAtom<Notes>('notes', {}, on => [
-    on(initNotes, (state, notes) => {
-        const notesMap: Notes = {}
-        notes.forEach(note => notesMap[note.noteId] = note)
-        return notesMap
-    }),
-    on(updateNote, (state, note) => ({
-        ...state,
-        [note.noteId]: note,
-    })),
-    on(removeNote, (state, noteId) => {
-        const newEvents = {
-            ...state,
-        }
-        delete newEvents[noteId]
-        return newEvents
-    }),
-])
 
 const orderedNotesAtom = map(
     notesAtom,
@@ -77,8 +48,8 @@ const sidebarNotesAtom = combine({
 
 const notesActions = {
     updateNote,
-    removeNote,
-    initNotes,
+    removeNotes,
+    updateNotes,
     setSelectedNote,
 }
 
