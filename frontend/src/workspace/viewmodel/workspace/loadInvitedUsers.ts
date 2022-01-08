90498c5763db38da034756fdb3da805dbd71dc1e
EldarMuhamethanov <eldarmy@mail.ru>
2022-01-08 15:53:55 +0300

    Добавил подгрузку овнера

--- frontend/src/workspace/viewmodel/workspace/loadInvitedUsers.ts
+++ frontend/src/workspace/viewmodel/workspace/loadInvitedUsers.ts
@@ -2,13 +2,16 @@ import {declareAsyncAction} from "../../../core/reatom/declareAsyncAction";
 import {WorkspaceApi} from "../../../api/workspaceApi";
 import {dispatchAsyncAction} from "../../../core/reatom/dispatchAsyncAction";
 import {loadAbsentUsers} from "../../../users/loadUsers";
+import {workspacesAtom} from "./workspace";
 
 
 const loadInvitedUsers = declareAsyncAction<string, Array<string>>('loadInvitedUsers',
     (workspaceId, store) => {
         return WorkspaceApi.getInvitedUsers(workspaceId)
             .then(async (users) => {
-                await dispatchAsyncAction(store, loadAbsentUsers, users)
+                const workspaces = store.getState(workspacesAtom)
+                const ownerId = workspaces[workspaceId].ownerId
+                await dispatchAsyncAction(store, loadAbsentUsers, [...users, ownerId])
                 return Promise.resolve(users)
             })
     }
