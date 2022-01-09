02e0d180ad28aa6b17fa243cb5064f08063f688b
EldarMuhamethanov <eldarmy@mail.ru>
2022-01-09 16:57:26 +0300

    Убрал флешбокс

--- frontend/src/currentUser/initUser.ts
+++ frontend/src/currentUser/initUser.ts
@@ -14,7 +14,6 @@ const initUserDataAction = declareAction((_, store) => {
         })
         .catch(() => {
             store.dispatch(setIsLoadingApp(false))
-            processStandardError()
         })
         .finally(() => {});
 });
